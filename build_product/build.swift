e720a59ea68b761c81071475c58361e795894558
郝志宇 <haozhiyu@ttmv.com>
2023-03-06 15:25:27 +0800

    ...

--- build_product/build.swift
+++ build_product/build.swift
@@ -10,7 +10,7 @@ import SwiftShell
 import CryptoSwift
 
 class build {
-    static let currentVersion = "1.4.2"
+    static let currentVersion = "1.4.3"
     
     var arguments: [String]
     var workingSpace: String = ""
